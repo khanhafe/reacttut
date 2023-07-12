5ac3e25915af09be4ec318ed705ceee2e0f77bc0
khanhafe <khanhafe@gmail.com>
2023-07-12 11:56:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Expense from "./components/Expense";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Expense />
+    </div>
+  );
+}
